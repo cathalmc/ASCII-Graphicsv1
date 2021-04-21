41ff394be5e6dbd60f4d6dfe980ae7f574df5042
Cathal McLoughlin <cathal.mc-loughlin@ucdconnect.ie>
2021-04-21 14:10:35 +0100

    Add files via upload

--- AsciGraphics.cpp
+++ AsciGraphics.cpp
@@ -1,5 +1,4 @@
-// AsciGraphics.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Real basic ASCII graphics
 
 #include <iostream>
 #include <cwchar>
@@ -13,7 +12,6 @@ void printline(int length,char pix = '#') {
     }
 
     std::cout << std::endl;
-
 }
 
 int main()
@@ -27,7 +25,6 @@ int main()
     cfi.FontWeight = FW_NORMAL;
     std::wcscpy(cfi.FaceName, L"Arial"); // Choose your font
     SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
-
 
     int size = 20;
 
@@ -45,14 +42,3 @@ int main()
     }
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
